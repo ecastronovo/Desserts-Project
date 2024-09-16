0cbdcb212d3a1e92756caa397d667d6241065ab5
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 15:26:49 -0700

    creating receipe view model

--- Desserts Project/Views/ReceipeView.swift
+++ Desserts Project/Views/ReceipeView.swift
@@ -1,8 +1,28 @@
 import SwiftUI
 
+class ReceipeViewModel: ObservableObject {
+    @Published var receipeProperties: ReceipeView.Properties?
+    @Published public var isLoading: Bool = false
+    private let networkManager: ReceipeNetworkManagerInterface
+    
+    public init(receipeProperties:  ReceipeView.Properties? = nil,
+                isLoading: Bool = false,
+                networkManager: ReceipeNetworkManagerInterface = ReceipeNetworkManager()) {
+        self.receipeProperties = receipeProperties
+        self.isLoading = isLoading
+        self.networkManager = networkManager
+    }
+}
+
 struct ReceipeView: View {
+    let mealId: String
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+
+    struct Properties {
+        
     }
 }
 
