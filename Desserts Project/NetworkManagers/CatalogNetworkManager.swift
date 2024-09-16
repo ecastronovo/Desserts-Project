c38072270976c88c2852c51fbf336d1cc5063adc
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 15:19:37 -0700

    having the network manager fetch the payload

--- Desserts Project/NetworkManagers/CatalogNetworkManager.swift
+++ Desserts Project/NetworkManagers/CatalogNetworkManager.swift
@@ -1,9 +1,28 @@
 import Foundation
 
 protocol CatalogNetworkManagerInterface {
-
+    func getMeals() async -> CatalogViewModel.Meals?
 }
 
 final class CatalogNetworkManager: CatalogNetworkManagerInterface {
+    let baseURLString = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
     
+    func getMeals() async -> CatalogViewModel.Meals? {
+        
+        guard  let url = URL(string: baseURLString) else { return nil }
+        
+        do {
+            // can be improved to look at response for error handling
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let jsonDecoder = JSONDecoder()
+            
+            let mealsResponse = try jsonDecoder.decode(CatalogViewModel.Meals.self, from: data)
+            
+            return mealsResponse
+        }
+        catch {
+            return nil
+        }
+    }
 }
+
