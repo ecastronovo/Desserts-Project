3dbe73e4d130888f0c92e388f720b250b87863d8
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 14:25:47 -0700

    Initial Commit

---
+++ Desserts Project/Desserts_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desserts_ProjectApp.swift
+//  Desserts Project
+//
+//  Created by Eric Castronovo on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Desserts_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
