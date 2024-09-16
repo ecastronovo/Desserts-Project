c0e38037aeeee8fc43c91925485f7dbedb507d6e
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 15:19:47 -0700

    calling fetch meals and sorting meals

--- Desserts Project/Views/CatalogView.swift
+++ Desserts Project/Views/CatalogView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 protocol CatalogViewModelInterface: ObservableObject {
-    func getMeals()
+    func getMeals() async
 }
 
 class CatalogViewModel: CatalogViewModelInterface {
@@ -17,7 +17,40 @@ class CatalogViewModel: CatalogViewModelInterface {
         self.networkManager = networkManager
     }
 
-    func getMeals() {
+    func getMeals() async {
+        await MainActor.run {
+            isLoading = true
+        }
+
+        let fetchedMeals = await networkManager.getMeals()
+        
+        guard let meals = fetchedMeals else {
+            await MainActor.run {
+                isLoading = false
+            }
+            return
+        }
+        
+        let sortedMeals = meals.meals.sorted(by: { firstMeal, secondMeal in
+            return firstMeal.strMeal ?? "" < secondMeal.strMeal ?? ""
+        })
+
+        let viewProperties: [CatalogView.Meal] = sortedMeals.compactMap { meal in
+            guard let name = meal.strMeal,
+                  let id = meal.idMeal,
+                  let urlString = meal.strMealThumb
+            else {
+                return nil
+            }
+            return CatalogView.Meal(mealName: name,
+                                    mealImageString: urlString,
+                                    mealId: id)
+        }
+        
+        await MainActor.run {
+            isLoading = false
+            catalogViewProperties = CatalogView.Properties(catalogMeals: viewProperties)
+        }
     }
 }
 
@@ -30,11 +63,38 @@ struct CatalogView: View {
                 ProgressView()
             }
             else {
-                
+                if let meals = catalogViewModel.catalogViewProperties?.catalogMeals {
+                    ScrollView {
+                        LazyVGrid(
+                            columns: [GridItem(spacing: 8), GridItem(spacing: 8)],
+                            spacing: 8
+                        ) {
+                            ForEach(meals, id: \.mealId) { meal in
+                                NavigationLink(destination: Text("Next View")) {
+                                    VStack {
+                                        Text("\(meal.mealName)")
+                                        AsyncImage(url: URL(string: meal.mealImageString)) { image in
+                                            image.resizable()
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .frame(width: 80, height: 80)
+                                    }
+                                }
+                                .frame(width: 140, height: 120)
+                                .padding(8)
+                                .border(.separator, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                                
+                            }
+                        }
+                    }
+                }
             }
         }
         .onAppear() {
-            catalogViewModel.getMeals()
+            Task {
+                await catalogViewModel.getMeals()
+            }
         }
     }
 
