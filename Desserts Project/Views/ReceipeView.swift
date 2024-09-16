5636965f71ed05b3a8e329291cf98d42b560ac16
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 15:42:02 -0700

    adding functionality to view model

--- Desserts Project/Views/ReceipeView.swift
+++ Desserts Project/Views/ReceipeView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 protocol ReceipeViewModelInterface: ObservableObject {
-    
+    func fetchReceipe(id: String) async
 }
 
 class ReceipeViewModel: ReceipeViewModelInterface {
@@ -16,11 +16,68 @@ class ReceipeViewModel: ReceipeViewModelInterface {
         self.isLoading = isLoading
         self.networkManager = networkManager
     }
+
+    func fetchReceipe(id: String) async {
+
+        await MainActor.run {
+            isLoading = true
+        }
+        
+        let mealResponse = await networkManager.fetchReceipe(id: id)
+        if let meal = mealResponse {
+            let viewProperties: ReceipeView.Properties? = getProperties(from: meal)
+            
+            await MainActor.run {
+                self.receipeProperties = viewProperties
+                isLoading = false
+            }
+        }
+        else {
+            await MainActor.run {
+                isLoading = false
+            }
+        }
+    }
+
+    private func getProperties(from payload: Meals) -> ReceipeView.Properties? {
+        guard let meal = payload.meals.first else { return nil }
+        let mealName = meal.strMeal ?? ""
+        var ingredients: [ReceipeView.Ingredient] = []
+        let mirrorMeal = Mirror(reflecting: meal)
+        
+        var dictionary: [String: String] = [:]
+        
+        let measurementsAndIngredients = mirrorMeal.children.filter { child in
+            (child.label?.contains("strMeasure") == true) || (child.label?.contains("strIngredient") == true)
+        }
+        
+        for child in measurementsAndIngredients {
+            guard let label = child.label?.description,
+                  let valueString = child.value as? String else {
+                continue
+            }
+            dictionary[label] = valueString
+        }
+        
+        for i in (1...20) {
+            let measurementKey = "strMeasure" + String(i)
+            let ingredientKey = "strIngredient" + String(i)
+            
+            if let measurementValue = dictionary[measurementKey],
+               let ingredientValue = dictionary[ingredientKey],
+               measurementValue != "" &&
+                ingredientValue != "" {
+                ingredients.append(ReceipeView.Ingredient(measurements: measurementValue, ingredientName: ingredientValue))
+            }
+        }
+        return .init(mealName: mealName, ingredients: ingredients)
+    }
 }
 
 struct ReceipeView: View {
+    @StateObject var receipeViewModel: ReceipeViewModel = ReceipeViewModel()
     let mealId: String
-
+    
     var body: some View {
         VStack {
             if receipeViewModel.isLoading {
