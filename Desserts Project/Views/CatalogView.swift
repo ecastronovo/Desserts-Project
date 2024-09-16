070f75aa2f1059408a5799e8e38ba897a5ab1822
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 14:32:31 -0700

    creating view model

--- Desserts Project/Views/CatalogView.swift
+++ Desserts Project/Views/CatalogView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
+class CatalogViewModel: ObservableObject {
+
+}
+
 struct CatalogView: View {
+    @StateObject var catalog: CatalogViewModel = CatalogViewModel()
+
     var body: some View {
         Text("")
     }
