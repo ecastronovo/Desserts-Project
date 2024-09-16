b258090792435aa3dce3c9292d7ad6be9a3aaf4f
Eric Castronovo <castronovo.eric@gmail.com>
2024-09-16 14:38:51 -0700

    adding manager to view model

--- Desserts Project/Views/CatalogView.swift
+++ Desserts Project/Views/CatalogView.swift
@@ -1,6 +1,17 @@
 import SwiftUI
 
 class CatalogViewModel: ObservableObject {
+    @Published var CatalogViewProperties: CatalogView.Properties?
+    @Published public var isLoading: Bool 
+    private let networkManager: CatalogNetworkManagerInterface
+
+    public init(CatalogViewProperties: CatalogView.Properties? = nil, 
+                isLoading: Bool = false,
+                networkManager: CatalogNetworkManagerInterface = CatalogNetworkManager()) {
+        self.CatalogViewProperties = CatalogViewProperties
+        self.isLoading = isLoading
+        self.networkManager = networkManager
+    }
 
 }
 
